d4f09ccdab84dffd58bedd89f48f7df90e6709dd
Radu Dita <hades2510@yahoo.com>
2016-03-17 18:55:43 +0200

    added HackerNewsComm

---
+++ NewsReader/HackerNewsComm.swift
@@ -0,0 +1,21 @@
+//
+//  HackerNewsComm.swift
+//  NewsReader
+//
+//  Created by Mac on 17/03/16.
+//  Copyright © 2016 mobileacademy. All rights reserved.
+//
+
+import UIKit
+
+class HackerNewsComm {
+    
+    func fetchLatestStories(){
+        
+    }
+    
+    func fetchTopStories(){
+        
+    }
+    
+}
