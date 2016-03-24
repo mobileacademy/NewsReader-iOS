593346308b7ee85a48cd107bb982382e04a155cc
Radu Dita <hades2510@yahoo.com>
2016-03-24 12:07:51 +0200

    removed hardcoded call to goToURL

--- NewsReader/StoryViewController.swift
+++ NewsReader/StoryViewController.swift
@@ -16,7 +16,6 @@ class StoryViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        goToUrl( "http://google.com" )
     }
 
     override func didReceiveMemoryWarning() {
