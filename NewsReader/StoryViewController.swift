cd447339c6fdadb44d7bee950f4f4855adee7a99
Radu Dita <hades2510@yahoo.com>
2016-03-24 11:30:48 +0200

    added StoryViewController

---
+++ NewsReader/StoryViewController.swift
@@ -0,0 +1,37 @@
+//
+//  StoryViewController.swift
+//  NewsReader
+//
+//  Created by Mac on 24/03/16.
+//  Copyright © 2016 mobileacademy. All rights reserved.
+//
+
+import UIKit
+
+class StoryViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
