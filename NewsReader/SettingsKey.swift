39ea7fe56f4e6b316f8d17e8cb35187b0e3f15c8
Radu Dita <hades2510@yahoo.com>
2016-04-01 10:25:46 +0300

    added enum for settings keys

---
+++ NewsReader/SettingsKey.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsKey.swift
+//  NewsReader
+//
+//  Created by Mac on 01/04/16.
+//  Copyright © 2016 mobileacademy. All rights reserved.
+//
+
+import Foundation
+
+enum SettingsKey:String{
+    case BG
+}
