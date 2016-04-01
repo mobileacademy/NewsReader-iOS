7b83c26fc1a7fe710279aae62f0c0e45e387278f
Radu Dita <hades2510@yahoo.com>
2016-04-01 10:15:56 +0300

    using NSUserDefaults for bg setting

--- NewsReader/SettingsTableViewController.swift
+++ NewsReader/SettingsTableViewController.swift
@@ -15,16 +15,20 @@ class SettingsTableViewController: UITableViewController {
     
     @IBAction func toggleBGSwitch(sender: UISwitch) {
         print( "\(sender.on)" )
+        
+        NSUserDefaults.standardUserDefaults().setBool(sender.on, forKey: "bg")
+        NSUserDefaults.standardUserDefaults().synchronize()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        bgSwtich.on = NSUserDefaults.standardUserDefaults().boolForKey("bg")
     }
 
     override func didReceiveMemoryWarning() {
