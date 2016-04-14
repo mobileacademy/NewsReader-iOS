27c01e967f24d25643d3459052ab19e5d7e2e141
Radu Dita <hades2510@yahoo.com>
2016-04-14 12:23:02 +0300

    added push notification end point

---
+++ NewsReader/PushAPI.swift
@@ -0,0 +1,14 @@
+//
+//  PushAPI.swift
+//  NewsReader
+//
+//  Created by Mac on 14/04/16.
+//  Copyright © 2016 mobileacademy. All rights reserved.
+//
+
+import Foundation
+
+class PushAPI{
+    static let addTokenEndpoint:String = "192.168.13.19/newsreaderpush/public/add_token/"
+    static let sendPushEndpoint:String = "192.168.13.19/newsreaderpush/public/send_notification/"
+}
