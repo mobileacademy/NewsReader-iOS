a781cd5bed427c1fb4f84fd1e6aac214be1a5fac
Radu Dita <hades2510@yahoo.com>
2016-04-14 12:55:56 +0300

    added url encoding

--- NewsReader/PushAPI.swift
+++ NewsReader/PushAPI.swift
@@ -10,10 +10,12 @@ import Foundation
 
 class PushAPI{
     static func addTokenEndpoint(token:String)->String{
-        return "192.168.13.19/newsreaderpush/public/add_token/\(token)"
+        let urlEncoded = token.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        return "http://192.168.13.19/newsreaderpush/public/add_token/\(urlEncoded)"
     }
     
     static func sendPushEndpoint(mess:String)->String{
-        return "192.168.13.19/newsreaderpush/public/send_notification/\(mess)"
+        let messEncoded = mess.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        return "http://192.168.13.19/newsreaderpush/public/send_notification/\(messEncoded)"
     }
 }
