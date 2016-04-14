e4c4b83a7e2b1790b3336ede5c38439fb11f6e54
Radu Dita <hades2510@yahoo.com>
2016-04-14 12:34:10 +0300

    added push api comm class

---
+++ NewsReader/PushComm.swift
@@ -0,0 +1,33 @@
+//
+//  PushComm.swift
+//  NewsReader
+//
+//  Created by Mac on 14/04/16.
+//  Copyright © 2016 mobileacademy. All rights reserved.
+//
+
+class PushComm: NSObject {
+    
+    func addToken( token:String ){
+        let url = NSURL( string:PushAPI.addTokenEndpoint(token) )!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url){
+            (data, response, err ) in
+            print(response)
+        }
+        
+        task.resume()
+    }
+    
+    func sendMessage( mess:String ){
+        let url = NSURL( string:PushAPI.sendPushEndpoint(mess) )!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url){
+            (data, response, err) in
+            print(resonse)
+        }
+        
+        task.resume()
+    }
+
+}
